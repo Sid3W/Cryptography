e6ce30f363d5540f7a3da82b9c13442b2dfd54ff
Xinyu Wu <88123403+Sid3W@users.noreply.github.com>
2022-09-07 16:42:24 +1000

    modify gcd to using library

--- gcd.py
+++ gcd.py
@@ -1,15 +1,4 @@
-def GCD(a,b):
-    if a == 0:
-        return b
-    if b == 0:
-        return a
-    if a >= b:
-        return GCD(a%b, b)
-    else:
-        return GCD(a, b%a)
-
-
-import sys
+import sys,math
 if len(sys.argv) == 1:
     a = int(input()) # if no input parameter, ask for an input number
     b = int(input())
@@ -17,4 +6,4 @@ else:
     a = int(sys.argv[1])
     b = int(sys.argv[2])
 
-print(GCD(a,b))
+print(math.gcd(a,b))
